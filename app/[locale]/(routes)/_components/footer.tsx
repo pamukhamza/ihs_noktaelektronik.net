01ca8aace606940a8e9951b86b180c2a3357f678
kececihasan <ghasankececi@gmail.com>
2024-12-24 14:30:40 +0300

    yeni

--- app/[locale]/(routes)/_components/footer.tsx
+++ app/[locale]/(routes)/_components/footer.tsx
@@ -28,15 +28,14 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-screen-xl container">
         <div className="grid grid-cols-2 gap-8 px-1 md:px-4 py-6 lg:py-8 md:grid-cols-4">
           <div>
-            <h2 className="mb-6 text-sm font-semibold  text-white uppercase ">{t('urunler')}</h2>
+            <h2 className="mb-6 text-sm font-semibold  text-white uppercase ">{t('cozumler')}</h2>
             <ul className="text-white text-sm">
-              <li className="mb-4"><a href="#" className=" hover:underline">Pasif Network Ürünleri</a></li>
-              <li className="mb-4"><a href="#" className=" hover:underline">Aktif Network Ürünleri</a></li>
-              <li className="mb-4"><a href="#" className=" hover:underline">CCTV Çözümleri</a></li>
+              <li className="mb-4"><a href="urunler/cctv-cozumleri-326" className=" hover:underline">CCTV Çözümleri</a></li>
+              <li className="mb-4"><a href="urunler/aktif-network-urunleri-2384" className=" hover:underline">Aktif Network Çözümleri</a></li>
+              <li className="mb-4"><a href="#" className=" hover:underline">Pasif Network Çözümleri</a></li>
               <li className="mb-4"><a href="#" className=" hover:underline">Hemşire Çağrı Sistemleri</a></li>
               <li className="mb-4"><a href="#" className=" hover:underline">Yangın Algılama Sistemleri</a></li>
               <li className="mb-4"><a href="#" className=" hover:underline">Bariyer ve Otopark Sistemleri</a></li>
-              <li className="mb-4"><a href="#" className=" hover:underline">About</a></li>
             </ul>
           </div>
           <div>
@@ -85,25 +84,25 @@ const Footer = () => {
         <div className="px-4 py-6 md:flex md:items-center md:justify-between border-t borderone">
           <span className="text-sm text-white sm:text-center">© 2024 <a href="https://www.noktaelektronik.net/">Nokta Elektronik</a>. {t('tum-haklar')}.</span>
           <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-            <a href="#" className="text-white">
+            <a href="https://www.facebook.com/nebsis/" className="text-white">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                 <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd"/>
               </svg>
               <span className="sr-only">Facebook page</span>
             </a>
-            <a href="#" className="text-white">
+            <a href="https://twitter.com/NEBSIS?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" className="text-white">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                 <path fillRule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clipRule="evenodd"/>
               </svg>
               <span className="sr-only">Twitter page</span>
             </a>
-            <a href="#" className="text-white">
+            <a href="https://www.instagram.com/noktaelektronik/" className="text-white">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                 <path fillRule="evenodd" d="M12 2c-2.716 0-3.056.012-4.123.06-1.064.049-1.791.218-2.427.465a4.901 4.901 0 0 0-1.772 1.153A4.902 4.902 0 0 0 2.525 5.45c-.247.636-.416 1.363-.465 2.427C2.012 8.944 2 9.284 2 12s.012 3.056.06 4.123c.049 1.064.218 1.791.465 2.427a4.902 4.902 0 0 0 1.153 1.772 4.901 4.901 0 0 0 1.772 1.153c.636.247 1.363.416 2.427.465 1.067.048 1.407.06 4.123.06s3.056-.012 4.123-.06c1.064-.049 1.791-.218 2.427-.465a4.902 4.902 0 0 0 1.772-1.153 4.902 4.902 0 0 0 1.153-1.772c.247-.636.416-1.363.465-2.427.048-1.067.06-1.407.06-4.123s-.012-3.056-.06-4.123c-.049-1.064-.218-1.791-.465-2.427a4.902 4.902 0 0 0-1.153-1.772 4.901 4.901 0 0 0-1.772-1.153c-.636-.247-1.363-.416-2.427-.465C15.056 2.012 14.716 2 12 2zm0 1.802c2.67 0 2.986.01 4.04.058.976.045 1.505.207 1.858.344.466.182.8.399 1.15.748.35.35.566.684.748 1.15.137.353.3.882.344 1.857.048 1.055.058 1.37.058 4.041 0 2.67-.01 2.986-.058 4.04-.045.976-.207 1.505-.344 1.858-.182.466-.399.8-.748 1.15-.35.35-.684.566-1.15.748-.353.137-.882.3-1.857.344-1.054.048-1.37.058-4.041.058-2.67 0-2.987-.01-4.04-.058-.976-.045-1.505-.207-1.858-.344a3.097 3.097 0 0 1-1.15-.748 3.098 3.098 0 0 1-.748-1.15c-.137-.353-.3-.882-.344-1.857-.048-1.055-.058-1.37-.058-4.041 0-2.67.01-2.986.058-4.04.045-.976.207-1.505.344-1.858.182-.466.399-.8.748-1.15.35-.35.684-.566 1.15-.748.353-.137.882-.3 1.857-.344 1.055-.048 1.37-.058 4.041-.058zm0 3.064a5.134 5.134 0 1 0 0 10.268 5.134 5.134 0 0 0 0-10.268zm0 8.466a3.332 3.332 0 1 1 0-6.664 3.332 3.332 0 0 1 0 6.664zm6.538-8.671a1.2 1.2 0 1 1-2.4 0 1.2 1.2 0 0 1 2.4 0z" clipRule="evenodd"/>
               </svg>
               <span className="sr-only">Instagram page</span>
             </a>
-            <a href="#" className="text-white">
+            <a href="https://www.linkedin.com/company/nokta-elektronik-ve-bili%C5%9Fim-sistemleri/" className="text-white">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                 <path fillRule="evenodd" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" clipRule="evenodd"/>
               </svg>
