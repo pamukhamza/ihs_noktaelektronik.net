09727173c1b6a09491920e95f62346c39936cc11
kececihasan <ghasankececi@gmail.com>
2024-12-25 11:02:23 +0300

    yeni

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+   /** @type {import('next-sitemap').IConfig} */
+   const config = {
+    siteUrl: 'https://noktaelektronik.net', // Your site's URL
+    generateRobotsTxt: true, // Generate robots.txt file
+    // Additional options can be added here
+  };
+
+  module.exports = config;
---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const withNextIntl = require('next-intl/plugin')('./i18n.ts');
+
+const nextConfig = {
+  images: {
+    domains: ['noktanetwork.com'], // Add your domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+}
+
+module.exports = withNextIntl(nextConfig);
