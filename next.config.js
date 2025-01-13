cc652a33b8cfa1aa0aeb381335bb550af9495b23
kececihasan <ghasankececi@gmail.com>
2025-01-13 17:58:31 +0300

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
+
 const nextConfig = {
   images: {
     domains: ['noktanetwork.com'], // Add your domain here
