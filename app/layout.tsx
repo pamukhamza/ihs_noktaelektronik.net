8f99e287822eb7e8f504a52a1f73b60278a3d1f6
pamukhamza <hmzpmk34@gmail.com>
2025-03-28 12:08:33 +0300

    google deneme

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-
+ 
         {children}
         <Toaster />
       </body>
